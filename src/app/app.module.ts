608c59a0d04555b12284c37012f3194f7ac2cd53
Craig Miller <craigmiller160@gmail.com>
2018-02-25 18:10:35 -0500

    Completed event binding tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyNewComponentComponent } from './my-new-component/my-new-component.component';
 import { BindingComponent } from './binding/binding.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MyNewComponentComponent,
-    BindingComponent
+    BindingComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent implements OnInit {
+
+  clickEvent(event) {
+    console.log('Click');
+    console.log(event);
+  }
+
+  mouseEnterEvent(event) {
+    console.log('Mouse enter');
+    console.log(event);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
